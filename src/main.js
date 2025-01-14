3a4d16b4833a09fc2e4b04f8a3954e343d899c8c
Runa Ozawa <G022C1013@g.neec.ac.jp>
2025-01-14 14:04:03 +0900

    main仮

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import { createApp } from 'vue';
-import Login from './AbsentDetails.vue';
-import router from './router';
-
-const app = createApp(Login);
-app.use(router);
-app.mount('#app'); // 必要なHTML要素IDを指定
-
 import { createApp } from 'vue'; 
 import App from './app.vue'; 
-import router from '../src/router/indesx.js'; 
+import router from '../src/router/index.js';
+
 createApp(App).use(router).mount('#app');
